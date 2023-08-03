5d8600bf0c73a28fcdf60f18c947db597b7b543f
user <user@DESKTOP-IS6DLKP>
2023-08-03 20:06:35 +0530

    main is committed

---
+++ src/test/java/DEMO/github2.java
@@ -0,0 +1,12 @@
+package DEMO;
+
+import org.testng.annotations.Test;
+
+public class github2 {
+@Test
+	public  void main() {
+		// TODO Auto-generated method stub
+System.out.println("dfsdgdfhdddg");
+	}
+
+}
